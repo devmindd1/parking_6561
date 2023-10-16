83b2ee7538019012e1a08e50bf2cf8b1f5d4bbc1
liketodev1 <freelancetoptalent@gmail.com>
2023-10-16 12:56:23 +0400

    u

--- config/database.js
+++ config/database.js
@@ -25,7 +25,7 @@
 module.exports = require('knex')({
     client: 'mysql2',
     connection: {
-        host:'localhost',
+        host:'134.209.113.29',
         user: 'avionest_my',
         database: 'parking',
         password: 's#a$d7+_98#453R',
